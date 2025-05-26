9e02d42de70f42cebe3615d9c5e59ced90affcfd
harshpa324 <harshpa324@gmail.com>
2025-05-26 20:45:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function productoftwo() {
+  let arr = [3, 4, 5, 6]
+
+  let res = []
+  for (let i = 0; i < arr.length; i++) {
+    res[i] = arr[(i + 1) % arr.length] * arr[(i + 2) % arr.length]
+  }
+  console.log(res)
+}
+
+productoftwo()
